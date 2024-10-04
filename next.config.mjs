cad5b203108aeea091d5a41c252c407ff3e3fdb1
Gard.Kalland <github.blunderer148@passmail.net>
2024-10-04 14:22:48 +0200

    added SWC minification

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
     images: {
       domains: ['cdn.sanity.io'], // Allow images from Sanity's CDN
     },
