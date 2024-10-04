470c51239dba575d826b444c5af884c4a5dfa1af
Gard.Kalland <github.blunderer148@passmail.net>
2024-10-04 13:54:24 +0200

    added runtime egde to sanity

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -14,6 +14,9 @@ export const dynamic = 'force-static'
 
 export { metadata, viewport } from 'next-sanity/studio'
 
+export const runtime = 'edge';
+
 export default function StudioPage() {
   return <NextStudio config={config} />
+
 }
