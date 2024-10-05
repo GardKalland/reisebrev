4ff4dd904e3778c302415b9fa0ccd511e6584973
Gard.Kalland <github.blunderer148@passmail.net>
2024-10-05 15:12:50 +0200

    changes some things in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-      domains: ['cdn.sanity.io'], // Allow images from Sanity's CDN
-    },
-  };
- console.log(process.env.NEXT_PUBLIC_SANITY_PROJECT_ID); 
-  export default nextConfig;
-  
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['cdn.sanity.io'], // Allow images from Sanity's CDN
+  },
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  },
+  webpack: (config) => {
+    // This will log during the build phase and runtime
+    console.log("Sanity Project ID:", process.env.NEXT_PUBLIC_SANITY_PROJECT_ID);
+    console.log("Sanity Dataset:", process.env.NEXT_PUBLIC_SANITY_DATASET);
+
+    return config;
+  }
+};
+
+export default nextConfig;
